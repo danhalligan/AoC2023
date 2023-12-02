2de83b4bf2f9f1244e5d0d03d2f2223937ea2249
Daniel Halligan <dan.halligan@gmail.com>
2023-12-02 10:38:43 +0000

    Update CLI

--- README.md
+++ README.md
@@ -9,32 +9,16 @@
 
 ## Example
 
-To solve a specific day, you can use `solve`, passing in the input file for
-the current day using poetry.
+To solve a specific day, you can use `solve`, passing in the day(s) to solve.
 
 ``` bash
-poetry run aoc2023 solve inputs/day01.txt
+poetry run aoc2023 solve 1 2
 ```
 
-Or pass in multiple days to solve multiple inputs.
+Or to solve all days (where available).
 
 ``` bash
-poetry run aoc2023 solve inputs/*
-```
-
-You can automatically download the input for a given day by setting your
-[session cookie] in an environment variable (or in a `.session.txt` text file
-in the working directory).
-
-``` bash
-export AOC_SESSION=[your session]
-```
-
-Then using the input command to get today's input:
-
-```python
-poetry run aoc2023 input
+poetry run aoc2023 solve
 ```
 
 [Advent of Code 2023]: https://adventofcode.com/2023
-[session cookie]: https://www.reddit.com/r/adventofcode/comments/a2vonl/how_to_download_inputs_with_a_script/
--- aoc2023/cli.py
+++ aoc2023/cli.py
@@ -1,27 +1,33 @@
 import typer
+from typing import List
 import importlib
+from datetime import datetime
 from aocd.models import Puzzle
 
 app = typer.Typer()
 
 
 @app.command()
-def solve(day: str):
-    """Solve a challenge for a given day"""
-    day = int(day)
-    module = importlib.import_module(f"aoc2023.day{day:02d}")
-    puzzle = Puzzle(year=2023, day=day)
-    print(f"--- Day {day}: {puzzle.title} ---")
+def solve(days: List[str] = list(range(1, 25))):
+    """Solve a challenge for given days"""
+    for day in days:
+        day = int(day)
+        module = importlib.import_module(f"aoc2023.day{day:02d}")
+        puzzle = Puzzle(year=2023, day=day)
+        if datetime.now(puzzle.unlock_time().tzinfo) < puzzle.unlock_time():
+            continue
 
-    try:
-        print("Part A:", getattr(module, "part_a")(puzzle.input_data))
-    except AttributeError:
-        print("No part A")
-    try:
-        print("Part B:", getattr(module, "part_b")(puzzle.input_data))
-    except AttributeError:
-        print("No part B")
-    print()
+        print(f"--- Day {day}: {puzzle.title} ---")
+
+        try:
+            print("Part A:", getattr(module, "part_a")(puzzle.input_data))
+        except AttributeError:
+            print("No part A")
+        try:
+            print("Part B:", getattr(module, "part_b")(puzzle.input_data))
+        except AttributeError:
+            print("No part B")
+        print()
 
 
 @app.command()
