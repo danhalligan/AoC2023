528ff77f7a130dceb0ed99fad56247a74495f652
Daniel Halligan <dan.halligan@gmail.com>
2023-12-04 07:01:44 +0000

    Simplified day 4

--- aoc2023/day04.py
+++ aoc2023/day04.py
@@ -4,31 +4,22 @@ import re
 def parse_data(data):
     lines = data.splitlines()
     for line in lines:
-        card, nums = line.split(": ")
+        _, nums = line.split(": ")
         winning, mine = nums.split(" | ")
-        winning = [int(x) for x in re.findall("\d+", winning)]
-        mine = [int(x) for x in re.findall("\d+", mine)]
-        yield winning, mine
-
-
-def n_winning(card):
-    return sum(num in card[0] for num in card[1])
+        winning = [int(x) for x in re.findall(r"\d+", winning)]
+        mine = [int(x) for x in re.findall(r"\d+", mine)]
+        yield sum(num in winning for num in mine)
 
 
 def part_a(data):
-    cards = list(parse_data(data))
-    tot = 0
-    for card in cards:
-        matches = n_winning(card)
-        tot += 2 ** (matches - 1) if matches else 0
-    return tot
+    scores = list(parse_data(data))
+    return sum(2 ** (score - 1) if score else 0 for score in scores)
 
 
 def part_b(data):
-    cards = list(parse_data(data))
-    card_counts = [1 for i in range(len(cards))]
-    for i in range(len(cards)):
-        nw = n_winning(cards[i])
-        for j in range(i + 1, i + 1 + nw):
+    scores = list(parse_data(data))
+    card_counts = [1 for _ in range(len(scores))]
+    for i in range(len(scores)):
+        for j in range(i + 1, i + 1 + scores[i]):
             card_counts[j] += card_counts[i]
     return sum(card_counts)
