618de0d50255762676707a3b32c8fc6868aa4fad
Daniel Halligan <dan.halligan@gmail.com>
2023-12-20 16:21:04 +0000

    Refactor day 20

--- aoc2023/modules.py
+++ aoc2023/modules.py
@@ -11,13 +11,13 @@ class Packet:
 def ModuleFactory(name, dests):
     """Factory Method"""
     if name == "broadcaster":
-        return Broadcaster("broadcaster", dests)
+        return Module("broadcaster", dests)
     elif name.startswith("%"):
         return FlipFlop(name[1:], dests)
     elif name.startswith("&"):
         return Conjunction(name[1:], dests)
     else:
-        return Module(name, dests)
+        return Module(name, [])
 
 
 class Module:
@@ -29,8 +29,16 @@ class Module:
         self.track = False
         self.inputs = rnetwork[self.name]
 
-    def exe(self, _):
-        return []
+    def calculate(self, packet):
+        return packet.val
+
+    def exe(self, packet):
+        val = self.calculate(packet)
+        if self.track and val == "high":
+            raise Exception("High found")
+        if val:
+            for dest in self.dests:
+                yield Packet(self.name, dest, val)
 
 
 class FlipFlop(Module):
@@ -39,12 +47,10 @@ class FlipFlop(Module):
         self.inputs = rnetwork[self.name]
         self.state = "low"
 
-    def exe(self, packet):
+    def calculate(self, packet):
         if packet.val == "low":
             self.state = "low" if self.state == "high" else "high"
-            for dest in self.dests:
-                # print(self.name, self.state, "->", dest)
-                yield Packet(self.name, dest, self.state)
+            return self.state
 
 
 class Conjunction(Module):
@@ -53,18 +59,6 @@ class Conjunction(Module):
         self.inputs = rnetwork[self.name]
         self.mem = {x: "low" for x in self.inputs}
 
-    def exe(self, packet):
+    def calculate(self, packet):
         self.mem[packet.fr] = packet.val
-        out = "low" if all(x == "high" for x in self.mem.values()) else "high"
-        if self.track and out == "high":
-            raise Exception("High found")
-        for dest in self.dests:
-            # print(self.name, out, "->", dest)
-            yield Packet(self.name, dest, out)
-
-
-class Broadcaster(Module):
-    def exe(self, packet):
-        for dest in self.dests:
-            # print("broadcaster", pulse, "->", dest)
-            yield Packet(self.name, dest, packet.val)
+        return "low" if all(x == "high" for x in self.mem.values()) else "high"
